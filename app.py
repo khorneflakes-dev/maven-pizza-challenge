8e9edb06f0811fbdff4669adca87bcc374cdd707
erick <98509698+khorneflakes-dev@users.noreply.github.com>
2022-10-05 00:44:37 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import pandas as pd
-import plotly.express as px
-
+from dash import dash, dcc, html
+import plotly_express as px
+from dash.dependencies import Input, Output
+import plotly.graph_objects as go
 # Aquí hay algunas preguntas que nos gustaría poder responder:
 
 # ¿Qué días y horas tendemos a estar más ocupados?
@@ -15,7 +17,7 @@ df_orders_details = pd.read_csv('./pizza_sales/order_details.csv', delimiter=','
 df_pizzas = pd.read_csv('./pizza_sales/pizzas.csv', delimiter=',')
 df_pizzas_types = pd.read_csv('./pizza_sales/pizza_types.csv', delimiter=',', encoding='latin1')
 
-# aniadiendo columnas de dia y hora 
+# aniadiendo columns de dia y hora 
 df_orders['day'] = pd.to_datetime(df_orders['date']).dt.day_name()
 df_orders['hour'] = pd.to_datetime(df_orders['time']).dt.hour
 
@@ -23,7 +25,6 @@ df_orders['hour'] = pd.to_datetime(df_orders['time']).dt.hour
 # monday, tuesday ... 0, 1, ...
 dias_ocupados = df_orders.groupby('day', as_index=False).agg({'order_id':'count'})
 horas_ocupadas = df_orders.groupby('hour', as_index=False).agg({'order_id':'count'})
-
 # estas graficas muestran cuantas pizzas estamos haciendo durante cada uno de las horas
 # fig = px.bar(dias_ocupados, x='day', y='order_id')
 
@@ -50,11 +51,305 @@ capacidad = pd.merge(df_orders_details, df_orders, left_on='order_id', right_on=
 demo = capacidad.sort_values(['hour','order_id'])
 # demo['aux'] = demo['quantity'] / demo['order_id']
 
-demo2 = (demo[['hour', 'order_id', 'order_details_id']])
+demo2 = demo[['hour', 'order_id', 'order_details_id']]
 
-demo3 = (demo2.groupby(['hour', 'order_id'], as_index=False).agg({'order_details_id':'count'}))
+demo3 = demo2.groupby(['hour', 'order_id'], as_index=False).agg({'order_details_id':'count'})
 
-demo4 = (demo3.groupby(['hour'], as_index=False).agg({'order_id':'count', 'order_details_id':'sum'}))
+demo4 = demo3.groupby(['hour'], as_index=False).agg({'order_id':'count', 'order_details_id':'sum'})
 demo4['avg'] = demo4['order_id'] / 15
 
-print(demo4)
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    html.Div([
+        html.Img(src='assets/pizzalogo.png', className='pizza-logo'),
+        html.H1("Plato's Pizza", className='title'),
+        html.Img(src='assets/mavenlogo.png', className='maven-logo')
+    ], className = 'banner'),
+
+    # row 1
+    html.Div([
+
+        html.Div([
+
+            html.Div([
+                dcc.RadioItems(id='dias-aux', value='', className='dias-aux'),
+                html.P('Dias mas ocupados', className='title-graph1'),
+
+            ], className='row1-column1-row1'),
+
+            html.Div([
+
+                dcc.Graph(id='dias_graph', figure={}, clickData=None)
+
+            ], className='row1-column1-row2')
+
+        ], className='row1-column1'),
+
+        html.Div([
+            
+            html.P('Pizzas Vendidas', className='pizzas-vendidas-title'),
+
+            html.P(id='pizzas-vendidas', className='pizzas-vendidas-count')
+
+        ], className='row1-column2'),
+
+        html.Div([
+
+            html.P('Horas mas ocupadas', className='title-graph2'),
+
+            html.Div(id='horas-ocupadas-dia', className='graph-day'),
+
+            html.Div([
+
+                dcc.Graph(id='horas-graph', figure={})
+
+            ], className='horas-graph')
+
+        ], className='row1-column3')
+
+    ], className='row1'),
+
+    html.Div([
+
+        html.Div([
+            html.P('Precio promedio de un pedido', className = 'avg-price-title'),
+            html.P(id='precio-promedio-pedido', className = 'avg-price-value')
+        ], className = 'row2-col1'),
+
+        html.Div([
+            
+        ], className = 'row2-col2'),
+
+        html.Div([
+
+
+        ], className = 'row2-col3')
+
+    ], className='row2'),
+
+    html.Div([
+
+        html.P('explicacion de la distribucion de asientos, el como se hizo',
+            className = 'distribution-explain'),
+
+        html.Div([
+
+            dcc.Graph(id='pie-distribution', figure={})
+        ], className='row3-col2'),
+
+        html.Div([
+
+            html.P('conclusion del por que de la grafica')
+
+        ], className = 'row3-col3')
+
+    ], className='row3'),
+
+    html.Div([
+
+        html.P('About me', className='about-me'),
+
+        html.Div([
+
+            html.Div([
+
+                html.Img(src='assets/github-logo.png', className='github-logo')
+
+            ], className = 'about-icon-1'),
+            
+            html.Div([
+
+                html.Img(src='assets/linkedin-logo.png', className = 'linkedin-logo')
+
+            ], className = 'about-icon-2')
+
+        ], className='about-icons')
+
+    ])
+
+], className = 'main-container')
+
+
+
+@app.callback(
+    Output('dias_graph', component_property='figure'),
+    [Input('dias-aux', component_property='value')]
+
+)
+
+def graph_dias(value):
+    dias_ocupados.columns = ['Day', 'Pizzas_Sold']
+    fig = px.bar(dias_ocupados, x='Day', y='Pizzas_Sold', template='plotly_dark',
+        color_discrete_sequence=['#E5C852'])
+    
+    fig.update_layout({
+        'plot_bgcolor': 'rgba(1, 1, 1, 0)',
+        'paper_bgcolor': 'rgba(0, 0, 0, 0)',
+        'xaxis_title': 'Weekdays',
+        'yaxis_title': 'Pizzas Sold',
+        'font_family': 'Lato',
+        'font_size': 20,
+        })
+    return fig
+
+@app.callback(
+    Output('horas-ocupadas-dia', component_property='children'),
+    [Input('dias_graph', component_property='clickData')]
+)
+
+def title_horas(clk_data):
+    if clk_data == None:
+        return 'Busiest Hours'
+    else:
+        clk = clk_data['points'][0]['x']
+        return clk
+
+
+@app.callback(
+    Output('horas-graph', component_property='figure'),
+    [Input('dias_graph', component_property='clickData')]
+)
+
+def graph_horas(clk_data):
+    if clk_data == None:
+
+        fig2 = go.Figure(
+            {
+                'data': [
+                    {
+                        'x': horas_ocupadas['hour'],
+                        'y': horas_ocupadas['order_id'],
+                        'mode': 'lines',
+                        'line': {'color': '#E5C852'},
+                        'stackgroup': 'one'
+                    }
+                ],
+
+                'layout': {
+                    "paper_bgcolor": "rgba(0,0,0,0)",
+                    "plot_bgcolor": "rgba(0,0,0,0)",
+                    'font_color': "rgb(212, 212, 212)",
+                    'font_family': 'Lato',
+                    'font_size': 20,
+                    'xaxis': dict(
+                            showline=False,
+                            showgrid=False,
+                            zeroline=False,
+                            showticklabels=True
+                    ),
+                    'yaxis': dict(
+                            showline=False,
+                            showgrid=True,
+                            zeroline=False,
+                            showticklabels=True
+                    ),
+                    'xaxis_title': '<b>Hours</b>',
+                    'yaxis_title': '<b>Pizzas Sold</b>',
+                },
+
+                'traces': {
+                    'line_shape': 'spline'
+                }
+            }
+        )
+
+
+
+        return fig2
+    else:
+        clk = clk_data['points'][0]['x']
+        horas_ocupadas_por_dia = df_orders[df_orders['day']==clk].groupby('hour', as_index=False).agg({'order_id':'count'})
+
+        fig2 = go.Figure(
+            {
+                'data': [
+                    {
+                        'x': horas_ocupadas_por_dia['hour'],
+                        'y': horas_ocupadas_por_dia['order_id'],
+                        'mode': 'lines',
+                        'line': {'color': '#E5C852'},
+                        'stackgroup': 'one'
+                    }
+                ],
+
+                'layout': {
+                    "paper_bgcolor": "rgba(0,0,0,0)",
+                    "plot_bgcolor": "rgba(0,0,0,0)",
+                    'font_color': "rgb(212, 212, 212)",
+                    'font_family': 'Lato',
+                    'font_size': 15,
+                    'xaxis': dict(
+                            showline=False,
+                            showgrid=False,
+                            zeroline=False,
+                            showticklabels=True
+                    ),
+                    'yaxis': dict(
+                            showline=False,
+                            showgrid=True,
+                            zeroline=False,
+                            showticklabels=True
+                    ),
+                    'xaxis_title': '<b>Hours</b>',
+                    'yaxis_title': '<b>Pizzas Sold</b>',
+                },
+
+                'traces': {
+                    'line_shape': 'spline'
+                }
+            }
+        )
+        return fig2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == ('__main__'):
+    app.run_server(debug=True)
